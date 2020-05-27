a29cd31fb490b322d7054a22646194ae2adf97b7
Apthornell <andrewthornellsound@gmail.com>
2020-05-27 16:44:21 -0500

    Added comments for both python scripts.

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,36 +2,44 @@ import os
 import csv
 
 
-
+#set import and export paths for files
 budget_csv = os.path.join('..','Resources','budget_data.csv')
 exportpath= os.path.join('..','Resources','financial_analysis.txt')
+#declare variables and empty lists, and set row and previous month values to 0
 months=[]
 profits=[]
 row=0
 previous_month=0
 net_change=[]
+
+#opens file and moves past the header
 with open(budget_csv, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
    
-    
+ #appends month list for every month it moves through, then appends the net_change list with the 
+ #difference between months taking into account the first iteration being 0 since there is not a previous
+ #month to look to. Also loads the profits as integars so that they can be used to calculate later.  
     for row in csvreader:
         months.append(row[0])
         profits.append(int(row[1]))
         if previous_month != 0: 
             net_change.append(int(row[1])-previous_month) 
         previous_month=int(row[1])
-
+#sets variables for the total months and gets calculation from the length of the months list
+#gets net profit by summing all values in profits list
 month_total=len(months)
 net_profit=sum(profits)
 
-
+#calculates the average change by summing the net change values, then dividing by total months. Rounded to two 
+#decimal places so that it would look nicer
 average_change=round(sum(net_change)/month_total,2)
 
 greatest_increase=profits[0]
 greatest_decrease=profits[0]
 
-
+#loops through and finds the greatest increase by comparing values of net change, then corrects the offset 
+#of the month since the loop is one behind the month it is looking at
 for i in range(len(net_change)):
     if net_change[i] >= greatest_increase:
         greatest_increase=net_change[i]
@@ -39,6 +47,7 @@ for i in range(len(net_change)):
     elif net_change[i]<= greatest_decrease:
         greatest_decrease=net_change[i]
         decrease_month=months[i+1]
+#set print statements and print to terminal
 output=(
     'Financial Analysis\n'
     '-----------------------------------\n'
@@ -49,7 +58,7 @@ output=(
     'Greatest decrease in profit =$'+ str(greatest_decrease)+ ' in '+ str(decrease_month)+'\n'
     'Total Profit= $'+ str(net_profit))
 print(output)
-
+#wrote print statements to txt file ouput path as well so that it wouldn't be needed to be typed twice
 with open(exportpath, 'w') as txt:
     txt.write(output)
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,22 +1,24 @@
 import os
 import csv
-
+#set file import and export paths
 electioncsv = os.path.join('..','Resources','election_data.csv')
 exportpath= os.path.join('..','Resources','election_results.txt')
-
+#start vote count at 0 and set empty dictionary to take candidate names and vote counts
 vote_count=0
 cand_unique={}
+#opens csv file and moves past header
 with open(electioncsv, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header=next(csvreader)
-    
+    #adds one to vote_count every time it loops through a row, then adds the candidate to dict if it isnt
+    #there already by searching for the key, and if it is there it adds one to the value.
     for row in csvreader:
       vote_count += 1
       if row[2] in cand_unique.keys():
         cand_unique[row[2]] += 1
       else:
         cand_unique[row[2]]= 1
-
+#writes header to txt file and prints to terminal, writes total vote count.
 with open(exportpath,'w') as txt:
   header=(
     "Election Results\n"
@@ -25,9 +27,13 @@ with open(exportpath,'w') as txt:
     "-------------------\n")
   print(header)
   txt.write(header)
+  #new dictionary to take candidate names with percentage of wins, as well as an empty string to recieve winner name
   win_percent={}
   winner=''
   greatest_value=0
+  #inside this for loop it calculates the candidates win percent using the cand_unique dictionary, and associated values
+  #it also prints their percentage as it goes to the terminal and the txt file. Then it finds the winner by comparing 
+  #number of votes for each candidate inside the cand_unique dictionary with the if statement
   for candidate in cand_unique:
     win_percent[candidate]=round(cand_unique.get(candidate)/vote_count*100,2)
     print(candidate +" "+ str(win_percent.get(candidate))+"%"+ " ("+ str(cand_unique.get(candidate))+")" )
